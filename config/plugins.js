a32a42bb0efcb2138b47b06e9c50df45b1721cf8
Viktor Gubarevich <viktor.gubarevich@zigzag.global>
2022-11-11 15:20:54 +0300

    Added changed names for variables.

--- config/plugins.js
+++ config/plugins.js
@@ -3,8 +3,8 @@ module.exports = ({ env }) => ({
     config: {
       provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env("AWS_ACCESS_KEY"),
-        secretAccessKey: env("AWS_SECRET_KEY"),
+        accessKeyId: env("AWS_ACCESS_KEY_ID_STRAPI"),
+        secretAccessKey: env("AWS_SECRET_ACCESS_KEY_STRAPI"),
         region: "us-east-1",
         params: {
           Bucket: env("AWS_BUCKET"),
